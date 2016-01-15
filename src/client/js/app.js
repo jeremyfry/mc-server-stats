404048ce5e06d5692e2558fbb12b286b8ede4ec3
Jeremy Fry <jeremy.m.fry@gmail.co>
2016-01-15 16:14:56 -0500

    wip

--- src/client/js/app.js
+++ src/client/js/app.js
@@ -1,5 +1,7 @@
+import '../css/bootstrap.css';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Nav from './nav';
+
 
 ReactDOM.render(<Nav />, document.querySelector('nav#main'));
--- webpack.config.js
+++ webpack.config.js
@@ -12,14 +12,13 @@ module.exports = {
 	module: {
 		loaders: [
 			{ test: /\.js?$/, loaders: ['react-hot', 'babel-loader'], exclude: /node_modules/ },
-			{ test: /\.css$/, loader: "style!css" }
+			{ test: /\.css$/, loader: "style!css" },
+			{ test: /\.(png|woff|woff2|eot|ttf|svg)$/, loader: 'url-loader?limit=100000' }
 		]
 	},
 	plugins: [
 	  new HtmlWebpackPlugin({
-		  template: '/src/client/index.html'
+		  template: './src/client/index.html'
 	  })
 	],
-
-
 };
